174a647e81305465f62c11d1f1fdbd0e481c36a4
Nikolaos Emmanouilidis <121699424+NikEmman@users.noreply.github.com>
2023-11-17 14:26:11 +0200

    Completed

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,10 +1,12 @@
 a=[4,3,78,2,0,2]
 def bubble_sort (arr)
-    for i in 0..arr.length-1
-        if arr[i] > arr[i+1]
-            arr.insert(i,arr.delete_at(i+1))
+    arr.length.times do 
+        for i in 0..arr.length-2
+            if arr[i] > arr[i+1]
+                arr.insert(i,arr.delete_at(i+1))
+            end
         end
     end
-    p arr
+p arr
 end
 bubble_sort(a)
